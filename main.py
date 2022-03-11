8cb1d2342a1684752d4d360bafaf6adfe038f05d
donaldssh <donald.shenaj@studenti.unipd.it>
2022-03-11 22:30:14 +0100

    added color_shape_approx method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pyautogui
 import time
-
 import pyscreenshot as ImageGrab
 import cv2
 import numpy as np
@@ -15,25 +14,41 @@ def template_matching(screen, button):
         location = maxloc[0] + int(w / 2), maxloc[1] + int(h / 2)
         return location
 
+def color_shape_approx(screen):
+    color1 = np.asarray([1, 1, 140])
+    color2 = np.asarray([10, 10, 200])
+    mask = cv2.inRange(screen, color1, color2)
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    for c in cnts:
+        perimeter = cv2.arcLength(c, True)
+        if perimeter > 100:
+            approx = cv2.approxPolyDP(c, 0.02 * perimeter, True)
+            if len(approx) == 4:
+                return cv2.boundingRect(approx)
 
-def screenrecording(args):
+def autoclicker(args):
     while True:
-        if args.multiscale:
+        location = None
+        if args.match == "color":
+            screen = cv2.cvtColor(np.array(ImageGrab.grab()), cv2.COLOR_BGR2RGB)
+            location = color_shape_approx(screen)
+        elif args.match == "template":
             screen = cv2.cvtColor(np.asarray(ImageGrab.grab()), cv2.COLOR_BGR2GRAY)
             button = cv2.imread("button.png", cv2.IMREAD_GRAYSCALE)
             location = template_matching(screen, button)
-        else:
+        elif args.match == "pyautogui":
             location = pyautogui.locateOnScreen("button.png")
 
         if location:
             print(f"Found button at {location}")
             pyautogui.click(location)
-            pyautogui.moveTo(location[0]+200, location[1]) 
-            
+            pyautogui.moveTo(location[0] + 400, location[1])
+
         time.sleep(1)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--multiscale", action="store_true")
-    screenrecording(parser.parse_args())
+    parser.add_argument("--match", type=str, choices=["color", "template", "pyautogui"], default="color")
+    autoclicker(parser.parse_args())
